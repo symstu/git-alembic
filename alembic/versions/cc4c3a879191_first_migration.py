3c3486993bcd13504428384500862858c228d850
Maksym Stukalo <stukalo.maksym@gmail.com>
2017-08-31 23:57:24 +0200

    test

---
+++ alembic/versions/cc4c3a879191_first_migration.py
@@ -0,0 +1,24 @@
+"""first_migration
+
+Revision ID: cc4c3a879191
+Revises: 
+Create Date: 2017-08-31 20:36:48.400958
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cc4c3a879191'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
