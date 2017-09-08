b74d50a57719ae1f3ded443c7d8e4d3e42631a25
alarsymstu <alarsymstu@alarstudios.com>
2017-09-08 14:08:08 +0200

    add 2 migrations

---
+++ alembic/versions/6cd3362d6eda_5.py
@@ -0,0 +1,24 @@
+""" 5
+
+Revision ID: 6cd3362d6eda
+Revises: abf6b43bfeb8
+Create Date: 2017-09-08 14:07:29.035430
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6cd3362d6eda'
+down_revision = 'abf6b43bfeb8'
+branch_labels = ('localization',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
