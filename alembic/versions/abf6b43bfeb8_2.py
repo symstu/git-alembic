278dca045078ee29bfb68b2301202b4c48ce4715
alarsymstu <alarsymstu@alarstudios.com>
2017-09-08 14:02:58 +0200

    first migration

---
+++ alembic/versions/abf6b43bfeb8_2.py
@@ -0,0 +1,24 @@
+""" 2
+
+Revision ID: abf6b43bfeb8
+Revises: c36f23a9c58b
+Create Date: 2017-09-08 12:34:09.981503
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'abf6b43bfeb8'
+down_revision = 'c36f23a9c58b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/versions/c36f23a9c58b_1.py
@@ -0,0 +1,24 @@
+""" 1
+
+Revision ID: c36f23a9c58b
+Revises: 
+Create Date: 2017-09-08 12:34:04.276130
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c36f23a9c58b'
+down_revision = None
+branch_labels = ('master',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
