79d13fcafa454e594430ff70a0b8071f62e9481f
alarsymstu <alarsymstu@alarstudios.com>
2017-09-08 15:12:12 +0200

    2 migrations

---
+++ alembic/versions/661f55e88801_3.py
@@ -0,0 +1,24 @@
+""" 3
+
+Revision ID: 661f55e88801
+Revises: f0b6a3420e18
+Create Date: 2017-09-08 15:10:33.846503
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '661f55e88801'
+down_revision = 'f0b6a3420e18'
+branch_labels = ('develop',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
