1db8320046399f75b6a50b849de2b6e0e4d6a8c6
alarsymstu <alarsymstu@alarstudios.com>
2017-09-08 16:15:05 +0200

    develop

---
+++ alembic/versions/943d01cec8db_3.py
@@ -0,0 +1,24 @@
+""" 3
+
+Revision ID: 943d01cec8db
+Revises: ea9f40881640
+Create Date: 2017-09-08 16:10:26.111672
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '943d01cec8db'
+down_revision = 'ea9f40881640'
+branch_labels = ('develop',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
