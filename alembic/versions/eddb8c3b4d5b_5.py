b1769763df3d06deb55fa502bb4b508c387b0293
alarsymstu <alarsymstu@alarstudios.com>
2017-09-08 15:14:46 +0200

    merge code

---
+++ alembic/versions/eddb8c3b4d5b_5.py
@@ -0,0 +1,24 @@
+""" 5
+
+Revision ID: eddb8c3b4d5b
+Revises: f0b6a3420e18
+Create Date: 2017-09-08 15:14:06.168535
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eddb8c3b4d5b'
+down_revision = 'f0b6a3420e18'
+branch_labels = ('localization',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
