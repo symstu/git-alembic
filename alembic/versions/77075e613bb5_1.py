55fe79bfd423383e6eabbe5f050b0873a1eb075a
alarsymstu <alarsymstu@alarstudios.com>
2017-09-08 16:09:04 +0200

    master

---
+++ alembic/versions/77075e613bb5_1.py
@@ -0,0 +1,24 @@
+""" 1
+
+Revision ID: 77075e613bb5
+Revises: 
+Create Date: 2017-09-08 16:06:33.691296
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '77075e613bb5'
+down_revision = None
+branch_labels = ('master',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
